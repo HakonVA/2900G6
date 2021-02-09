c7da3a92160db9a3f87e083707fcbd604fcd362d
jos019 <juhan955@gmail.com>
2021-02-09 11:48:59 +0100

    init commit for ingredients app

---
+++ project/ingredients/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+class IngredientsModelTests(TestCase):
+    """[summary]
+    
+        Creating/updating/deleting of models, model methods, model manager methods.
+
+    Args:
+        TestCase ([type]): [description]
+    """
+    
+    def setUp(self):
+        pass
