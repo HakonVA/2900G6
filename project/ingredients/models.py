529f47d96e21ac154af22e5ffa3c684b3422f7e4
JuhanOstby <Juhan955@gmail.com>
2021-02-12 12:09:29 +0100

    Sample ingredient and nutrient models

--- project/ingredients/models.py
+++ project/ingredients/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+    """
+    Food
+        fd_id               | Unique permanent identifier of the food
+        fd_category_id      | Id of the food category the food belongs to
+        scientific_name     | The scientific name for the food
+        description         | Description of the food
+    """
+    fd_id = models.CharField(max_length=32, primary_key=True)  
+    fd_category_id = models.CharField(max_length=32)                        
+    scientific_name = models.CharField(max_length=32)                       
+    description = models.CharField(max_length=255)  
+
+    def __str__(self):
+        return "fd_id: %s name: %s" % (self.fd_id, self.scientific_name) 
+        
+class Food_nutrient(models.Model):
+    """
+    Food_nutrient
+        id                  
+        fd_id               | ID of the food this food nutrient pertains to
+        amount/unit         | Amount of the nutrient per 100g of food. Specified in unit defined in the nutrient table.
+        protein_value       | The factor for protein
+        fat_value           | The factor for fat
+        carbohydrate_value  | The factor for carbohydrates
+    """
+    fd_id = models.ForeignKey(Food, on_delete=models.CASCADE)
+    amount = models.CharField(max_length=32)
+    protein_value = models.CharField(max_length=32)
+    fat_value = models.CharField(max_length=32)
+    carbohydrate_value = models.CharField(max_length=32)
+    
+    def __str__(self):
+        return "prot: %s fat: %s carb: %s" % (self.protein_value, self.fat_value, self.carbohydrate_value)
