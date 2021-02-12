1b6c35fb90adaee62be84acccbffef7d34f1b2dd
JuhanOstby <Juhan955@gmail.com>
2021-02-12 15:24:20 +0100

    Css, html and javascript

--- project/ingredients/urls.py
+++ project/ingredients/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import TemplateView
 
 # from .views import (
 
 # )
 
 urlpatterns = [
-    
+    path('', TemplateView.as_view(template_name='ingredients/ingredients-list.html'), name='ingred-list'),
 ]
