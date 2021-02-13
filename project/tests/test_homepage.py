d547af1d52e5b08448caf93ff418eaa3580027f6
egr054 <egr054@ifilab76-lnx.stud.cs.uit.no>
2021-02-13 14:12:21 +0100

    tests homepage

--- project/tests/test_homepage.py
+++ project/tests/test_homepage.py
@@ -1,36 +1,43 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import TestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-homepage_title = "Homepage" #used to determine if homepage is built correctly
-
-#import info for tests, all methods need test_.. etc in the name
+homepage_title = "Project Name" #the project name, update when changed
 
 class HomepageTest(TestCase):
-    
     #test for asserting that we are using the homepage template
     def test_homepage(self):
         response = self.client.get(f'')
         self.assertTemplateUsed(response, 'pages/home.html')
 
 class VisitPageTest(StaticLiveServerTestCase):
-    #test for asserting that the homepage title is correct
+    #tests for asserting that the homepage has the correct information
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.get(f'{self.live_server_url}')
+        self.homepage_url = self.live_server_url
+
+    def tearDown(self):
+        self.browser.quit()
+    
     def test_homepage_title(self):
+        self.assertEqual(self.browser.title, homepage_title)
 
-        #running on ubuntu terminal, no display        
-        browser = webdriver.Firefox()
-
-        browser.get(f'{self.live_server_url}')
-
-        browser.quit()
-
-
-
-
+    def test_button_login(self):
         
+        login_button = self.browser.find_element_by_class_name("Login")
+        
+        login_button.send_keys(Keys.ENTER)
 
-#assert that homepage has title "homepage"
-#find title
-#with views?
+        self.assertEqual(f'{self.homepage_url}/login', self.live_server_url)
 
+    def test_button_ingredients(self):
+        
+        ingredients_button = self.browser.find_element_by_class_name("Ingredients")
+
+        ingredients_button.send_keys(Keys.ENTER)
+
+        self.assertEqual(f'{self.homepage_url}/ingredients', self.live_server_url)
 
