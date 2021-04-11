10d96652f2b4f210c9854b22fc76ff974782f159
JuhanOstby <Juhan955@gmail.com>
2021-04-11 08:50:00 +0200

    make clean

--- project/ingredients/urls.py
+++ project/ingredients/urls.py
@@ -2,12 +2,10 @@ from django.urls import include, path
 
 from .views import (
     pantry_index_view,
-    pantry_DeleteAll_view,
-    # pantry_list_view,
-    # pantry_create_view,
+    pantry_delete_all_view,
 )
 
 urlpatterns = [
     path('', view=pantry_index_view, name='pantry-home'),
-    path('deleteAll', view=pantry_DeleteAll_view, name='Delete-all'),
+    path('delete-all', view=pantry_delete_all_view, name='delete-all'),
 ]
