1d421f31d528d2e8f8a6c05606f964ca3cc1a0c0
JuhanOstby <Juhan955@gmail.com>
2021-04-21 21:37:47 +0200

    update

--- project/pantrys/urls.py
+++ project/pantrys/urls.py
@@ -1,10 +1,16 @@
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-# from .views import (
-   
-# )
+from .views import (
+   user_ingredient_list_view,
+)
 
 urlpatterns = [
-    path('pantrys/', TemplateView.as_view(template_name='pantrys/pantrys_detail.html'), name='pantrys-detail'),
+    path('pantrys/', view=user_ingredient_list_view, name='pantrys-list'),
 ]   
+
+# http://127.0.0.1:8000/pantrys/
+# http://127.0.0.1:8000/pantrys/add
+# http://127.0.0.1:8000/pantrys/<int:pk>
+# http://127.0.0.1:8000/pantrys/<int:pk>/update
+# http://127.0.0.1:8000/pantrys/<int:pk>/delete
--- project/pantrys/views.py
+++ project/pantrys/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from .models import UserIngredient
+from project.recipes.models import Recipe
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView,
+    CreateView,
+)
+
+
+class UserIngredientListView(LoginRequiredMixin, ListView):
+    model = UserIngredient
+    login_url = 'login'
+    template_name = "pantrys/pantrys_list.html"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        ingredient_list = context['object_list']
+        recipe_list = Recipe.objects.all()
+
+        user_food_id = [obj.food.id for obj in ingredient_list]
+        print(user_food_id)
+
+        recipe_food_id = []
+        for recipe in recipe_list:
+            for obj in recipe.ingredients.all():
+                recipe_food_id.append(obj.food.id)
+    
+        print(recipe_food_id)
+
+        print()
+        print(Recipe.objects.filter(ingredients__food_id__in=user_food_id))
+        print()
+
+        context['recipes_list'] = Recipe.objects.filter(ingredients__food_id__in=user_food_id)
+        print(context)
+        return context
+
+user_ingredient_list_view = UserIngredientListView.as_view()
