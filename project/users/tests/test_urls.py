5a70f40e6a7844836604829ba0f719abe4e7d2ec
Granbark <egr054@uit.no>
2021-04-30 18:47:57 +0200

    some more tests

--- project/users/tests/test_urls.py
+++ project/users/tests/test_urls.py
@@ -1,1 +1,30 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+class LoginViewTest(TestCase):
+
+    #helper method for asserting templates
+    def assert_templates(self, url, template_names):
+        response = self.client.get(url)
+        for template in template_names:
+            self.assertTemplateUsed(template)
+
+    def test_logout_redirects_to_login(self):
+        response = self.client.get("/logmeout")
+        self.assertRedirects(response, "/login")
+
+    def test_logoutpage_redirects_to_login(self):
+        response = self.client.get("/logoutpage")
+        self.assertRedirects(response, "/login")
+    
+    def test_login_uses_correct_template(self):
+        templates = ["base.html", "/users/loginpage.html"]
+        self.assert_templates("/login", templates)
+
+    def test_signup_uses_correct_template(self):
+        templates = ["base.html", "/users/signuppage.html"]
+        self.assert_templates("/signup", templates)
+        
+    def test_logout_uses_correct_template(self):
+        templates = ["base.html", "/users/logoutpage.html"]
+        self.assert_templates("/logoutpage", templates)
