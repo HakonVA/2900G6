fa5ebd9df51e2286f1d3c9a3d241046a64d422a5
Granbark <egr054@uit.no>
2021-05-09 23:41:59 +0200

    more tests for recipes

--- project/recipes/tests/test_views.py
+++ project/recipes/tests/test_views.py
@@ -49,7 +49,7 @@ class ViewRecipiesTest(TestCase):
         try: 
             assert(response.status_code == 200)
         except AssertionError:
-            print("database not defined")
+            raise
 
         #test add to shopping here
 
@@ -61,61 +61,4 @@ class ViewRecipiesTest(TestCase):
         # create a recipe without a image and see how it works
         # use setupMissing_image
         # and check if anything else will bonk it
-
-
-#made just to test to see if could create an admin post of database item
-class StuffTest(TestCase):
-    def createRecipe(self):
-        
-        self.client = Client()
-        #c = Client()
-        self.client.post('/login', {'username': 'admin', 'password': '123'})
-
-        new_food = Food(name='sei')
-        new_food.save()
-
-        #response = self.client.post('/admin/recipes/food/add/', {'name' : "sei"})
-""" 
-        print(response)
-        assert(response.status_code == 302)
- """
-        new_ingredient = Ingredient(food=new_food, amount=5, unit='g')
-        new_ingredient.save()
-""" 
-        response = self.client.post('/admin/recipes/ingredient/add/',{
-            'food':new_food,
-            'Amount':5,
-            'Unit':'g'
-        })
- """
-"""         print(response)
-        assert(response.status_code == 302)
-
- """
-"""         self.client.post('/admin/recipes/recipe/add/',{
-            'name' : "fiskekaker",
-            'Ingredients': new_ingredient
-        })
-        print(response)
-        assert(response.status_code == 302)
-
- """
-    new_recipe = Recipe(name="fiskekaker")
-    new_recipe.ingredients.add(new_ingredient)
-    new_recipe.save()
-
-    def setUp(self):
-        
-        self.super_user = User.objects.create_superuser('admin', 'admin@example.com', '123')
-
-    def test_recipe(self):
-        self.createRecipe()
-#        response = self.client.post('/login', {'username':'admin','password':'123'})
-  #      print(response)
- #       response = self.client.get('/login')
-   #     print(response)
-        response = self.client.get('/recipes/')
-        print(response)
-        response = self.client.get('/recipes/1/')
-        print(response)
         
