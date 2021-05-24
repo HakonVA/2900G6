2defd51379c7df0a370926d719c06e59d6404de8
Granbark <egr054@uit.no>
2021-05-24 18:13:11 +0200

    recipe view tests

--- project/recipes/tests/test_views.py
+++ project/recipes/tests/test_views.py
@@ -6,6 +6,7 @@ from project.recipes.models import Recipe
 from project.recipes.models import Food
 from project.recipes.models import Ingredient
 from project.users.tests.test_views import createUser
+from project.pantrys.models import UserIngredient
 
 
 #recipe views:
@@ -49,23 +50,89 @@ class ViewRecipiesTest(TestCase):
         response = c.get('/recipes/1/add_to_shopping')
         assert(response.status_code == 302 or response.status_code == 301)
 
-    #a test to view the page if missing image
-    #and potenially other faults in recipes
-    def test_recipe_missing_image(self):
-        #TODO:
-        pass
-        # create a recipe without a image and see how it works
-        # use setupMissing_image
-        # and check if anything else will bonk it
-
     def test_recipe_detail_view(self):
-        #TODO: major
-        pass
+        c = Client()
+        createUser(c, "TestUser", "Hallo123@")
+
+        response = c.get('/recipes/1/')
+        self.assertTemplateUsed(response, 'recipes/recipes_detail.html')
 
     def test_recipe_list_view(self):
-        #TODO: major
-        pass
+        c = Client()
+        createUser(c, "TestUser", "Hallo123@")
 
-    def test_recipe_to_shopping_list(self):
-        #TODO: major
-        pass
+        response = c.get('/recipes/')
+        self.assertTemplateUsed(response, 'recipes/recipes_list.html')
+
+    #test 1:
+    #empty ingredient list
+    #test all added
+    def test_recipe_to_shopping_list1(self):
+        
+        c = Client()
+        createUser(c, "TestUser", "Hallo123@")
+
+        response = c.get('/pantrys/ingredients/')
+        assert(len(response.context['object_list']) == 0)
+
+        response = c.get('/recipes/1/add_to_shopping/')
+        c.get('/shopping/checkout/')
+
+        response = c.get('/pantrys/ingredients/')
+        assert(len(response.context['object_list']) == 6)
+
+    #test 2:
+    #partly full ingredient list
+    #test amount added is correct
+    def test_recipe_to_shopping_list2(self):
+
+        c = Client()
+        createUser(c, "TestUser", "Hallo123@")
+        user = User.objects.get(username="TestUser")
+
+        response = c.get('/pantrys/ingredients/')
+        assert(len(response.context['object_list']) == 0)
+
+        """ 400 g cauliflower
+        5 g oil
+        5 g fennel seeds
+        150 g red lentils
+        30 g curry paste 
+        20 g lemon juice """
+
+        cauliflower_food = Food.objects.get(name="cauliflower")
+        oil_food = Food.objects.get(name="oil")
+        lemonjuice_food = Food.objects.get(name="lemon juice")
+
+        #too little
+        UserIngredient.objects.create(food=cauliflower_food, user=user, amount=200)
+        
+        #too little
+        UserIngredient.objects.create(food=oil_food, user=user, amount = 2)
+        
+        #too much
+        UserIngredient.objects.create(food=lemonjuice_food, user=user, amount = 40)
+
+        c.get('/recipes/1/add_to_shopping/')
+        c.get('/shopping/checkout/')
+
+        response = c.get('/pantrys/ingredients/')
+
+        response_objects = response.context['object_list']
+
+        for objects in response_objects:
+            if objects.food == "cauliflower":
+                assert(objects.amount == 400)
+            if objects.food == "oil":
+                assert(objects.amount == 5)
+            if objects.food == "lemon juice":
+                assert(objects.amount == 40)
+
+
+
+
+
+
+
+
+
